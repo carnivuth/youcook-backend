59f029ea20e852beb840e5d05b411c5c01a98534
carnivuth <matti200042@gmail.com>
2023-08-10 00:00:52 +0200

    added dotenv

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
@@ -5,10 +6,10 @@ const PORT = 8039;
 //dbsetup
 const mysql = require("mysql");
 const connection = mysql.createConnection({
-  host: "homestead",
-  user: "homestead",
-  password: "secret",
-  database: "youcook",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect();
