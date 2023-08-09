de52ec4d1504e1eff190328a3bbc0d4643bb1f70
carnivuth <matti200042@gmail.com>
2023-08-09 23:40:47 +0200

    added mok data

--- migrations/MOK_DATA.sql
+++ migrations/MOK_DATA.sql
@@ -10,4 +10,10 @@ insert into INGREDIENTS(name)values ("zucchero");
 insert into INGREDIENTS(name)values ("carote");
 insert into INGREDIENTS(name)values ("carote");
 insert into INGREDIENTS(name)values ("carote");
-insert into RECIPES(name)values ("ricetta a")
+insert into RECIPES(name)values ("ricetta a");
+insert into QUANTITIES(id_recipe, id_ingredient) values (1,1);
+insert into QUANTITIES(id_recipe, id_ingredient) values (1,2);
+insert into STEPS(name, description)values ("step1","eseguire step1");
+insert into RECIPE_STEPS(id_recipe, id_step)values (1,1);
+
+select * from STEPS left join RECIPE_STEPS  on RECIPE_STEPS.id_step = STEPS.id where RECIPE_STEPS.id_recipe=1
