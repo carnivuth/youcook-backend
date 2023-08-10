af4c03219f2259a2966bb96c2e79039f8bd72c50
carnivuth <matti200042@gmail.com>
2023-08-10 20:31:41 +0200

    added fields

--- migrations/DEFAULT_SCHEMA.sql
+++ migrations/DEFAULT_SCHEMA.sql
@@ -1,8 +1,8 @@
-
 create or replace table RECIPES
 (
-    id        int primary key UNIQUE auto_increment,
-    name      varchar(50) not null,
+    id           int primary key UNIQUE auto_increment,
+    name         varchar(50)   not null,
+    description  varchar(2000) NOT NULL,
     instructions varchar(2000) default NULL
 );
 
@@ -16,8 +16,9 @@ create or replace table INGREDIENTS
 
 create or replace table QUANTITIES
 (
-    id_recipe     int not null  references RECIPES(id),
-    id_ingredient int not null  references INGREDIENTS(id)
+    id_recipe     int          not null references RECIPES (id),
+    id_ingredient int          not null references INGREDIENTS (id),
+    quantity      varchar(200) not null
 );
 
 create or replace table STEPS
@@ -28,13 +29,6 @@ create or replace table STEPS
 );
 create or replace table RECIPE_STEPS
 (
-    id_recipe     int not null  references RECIPES(id),
-    id_step int not null  references STEPS(id)
+    id_recipe int not null references RECIPES (id),
+    id_step   int not null references STEPS (id)
 );
-
-
-select * from RECIPES
-left join QUANTITIES on QUANTITIES.id_recipe=RECIPES.id
-left join INGREDIENTS I on QUANTITIES.id_ingredient = I.id
-left join RECIPE_STEPS RS on RECIPES.id = RS.id_recipe
-left join STEPS S on RS.id_step = S.id
