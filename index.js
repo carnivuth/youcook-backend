c970cd9a4eec3e79129f05cb95b6dea7c2d4f268
carnivuth <matti200042@gmail.com>
2023-08-10 20:32:19 +0200

    fixed cors request issue

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
+var cors = require('cors');
+app.use(cors());
 const PORT = process.env.API_PORT;
 //dbsetup
 const mysql = require("mysql");
