39af8544f08fabf65952104086b847ab0c699879
carnivuth <matti200042@gmail.com>
2023-08-13 20:16:59 +0200

    added mok data

--- migrations/MOK_DATA.sql
+++ migrations/MOK_DATA.sql
@@ -10,9 +10,19 @@ insert into INGREDIENTS(name)values ("zucchero");
 insert into INGREDIENTS(name)values ("carote");
 insert into INGREDIENTS(name)values ("carote");
 insert into INGREDIENTS(name)values ("carote");
+insert into INGREDIENTS(name)values ("patate");
+insert into RECIPE_STEPS (id_recipe, id_step) values (3,2);
+insert into RECIPE_STEPS (id_recipe, id_step) values (3,3);
+insert into RECIPE_STEPS (id_recipe, id_step) values (3,4);
 insert into RECIPES( name, instructions, description)values ("patate sabbiose",null,"patate al forno con pangrattato");
+insert into STEPS( name, description) values ("tagliare le patate","tagliare le patate con una mandolina");
+insert into STEPS( name, description) values ("condire","condire le patate con sale olio e pangrattato");
+insert into STEPS( name, description) values ("cottura","cuocere in forno a 200 gradi fino a doratura");
 insert into QUANTITIES(id_recipe, id_ingredient) values (1,1);
 insert into QUANTITIES(id_recipe, id_ingredient) values (1,2);
+insert into QUANTITIES(id_recipe, id_ingredient,quantity) values (3,12,"500 grammi");
+insert into QUANTITIES(id_recipe, id_ingredient,quantity) values (3,1,"500 grammi");
+
 insert into STEPS(name, description)values ("step1","eseguire step1");
 insert into RECIPE_STEPS(id_recipe, id_step)values (1,1);
 
