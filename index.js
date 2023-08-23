f56789714e29778b5344b4eda6b5fb7419d6dee4
carnivuth <matti200042@gmail.com>
2023-08-23 16:08:10 +0200

    changed mysql dependency to mysql2

--- index.js
+++ index.js
@@ -18,12 +18,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //db setup
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port:process.env.DB_PORT
 });
 connection.connect();
 
