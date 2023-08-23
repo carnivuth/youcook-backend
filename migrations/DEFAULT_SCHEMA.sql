068d9378d35974629246dcde07cc1dcf5aaa60f1
carnivuth <matti200042@gmail.com>
2023-08-23 16:08:23 +0200

    changed dialect to mysql

--- migrations/DEFAULT_SCHEMA.sql
+++ migrations/DEFAULT_SCHEMA.sql
@@ -1,34 +1,22 @@
-create or replace table RECIPES
+create table  RECIPES
 (
-    id           int primary key UNIQUE auto_increment,
+    id           INT primary key UNIQUE auto_increment,
     name         varchar(50)   not null,
     description  varchar(2000) NOT NULL,
     instructions varchar(2000) default NULL
 );
 
 
-create or replace table INGREDIENTS
+create  table INGREDIENTS
 (
     id   int primary key UNIQUE auto_increment,
     name varchar(50) not null
 
 );
 
-create or replace table QUANTITIES
+create  table QUANTITIES
 (
     id_recipe     int          not null references RECIPES (id),
     id_ingredient int          not null references INGREDIENTS (id),
     quantity      varchar(200) not null
-);
-
-create or replace table STEPS
-(
-    id          int primary key UNIQUE auto_increment,
-    name        varchar(50) not null,
-    description varchar(50) not null
-);
-create or replace table RECIPE_STEPS
-(
-    id_recipe int not null references RECIPES (id),
-    id_step   int not null references STEPS (id)
 );
